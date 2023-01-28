728693e3cdef54ad3002a1a1af31cde1c9c74c5b
redwin-0000 <redwin125896@gmail.com>
2023-01-28 10:18:01 +0530

    update live link

--- src/components/Project.js
+++ src/components/Project.js
@@ -42,7 +42,7 @@ return(
           <h1 className="title-font text-lg font-medium text-gray-500 mb-3">Authentication with Firebase</h1>
           <p className="leading-relaxed mb-3 text-gray-900 text-xl">Technologies Used <span className="text-blue-700">ReactJs, CSS, JavaScript</span></p>
           <div className="flex items-center flex-wrap gap-36">
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://github.com/redwin-0000/signIn">Live
+            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://authe-firebase.netlify.app/">Live
               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M5 12h14"></path>
                 <path d="M12 5l7 7-7 7"></path>
