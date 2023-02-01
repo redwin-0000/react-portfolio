3cb1531fd6bb0f231941226968c1f7f893674b8b
redwin-0000 <redwin125896@gmail.com>
2023-02-01 13:22:25 +0530

    font change

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header=()=> {
     <header className="text-gray-600 body-font top-0 sticky bg-gray-300">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <span className="ml-3 text-xl font-bold">Ashutosh Singh</span>
+        <span className="ml-3 text-xl font-bold">ASHUTOSH SINGH</span>
       </div>
       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
         <h2 className="mr-5 hover:text-gray-900 cursor-pointer" onClick={()=>{
