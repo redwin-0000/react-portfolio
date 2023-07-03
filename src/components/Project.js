0f947cb07949e76e89a6ac6fb448faad163eed53
redwin-0000 <redwin125896@gmail.com>
2023-07-03 18:02:55 +0530

    add functionality loadmore in project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,36 @@
-import React from "react";
-import ecimage from "../components/image/eg.png"
-import rodeImage from "../components/image/rode.png"
-import auth from "../components/image/auth.png"
+import React,{useState} from "react";
+import Data from "./data";
 const Project =()=>{
+  const [noOfElement, setnoOfElement] =  useState(3);
+
+  const slice = Data.cardData.slice(0,noOfElement);
+ const loadMore = ()=>{
+  setnoOfElement(noOfElement + noOfElement);
+ }
+
 return(
 <section id="project" className="text-gray-600 body-font bg-gray-200">
 <h2 className="sm:text-3xl text-center  text-2xl text-gray-900">PROJECT</h2>
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap -m-4">
 
-      <div className="p-4 md:w-1/3">
+  {
+    slice.map((items, index)=>{
+   return(
+    <div key={index} className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={ecimage} alt="e-commerce" />
+        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={items.img} alt="e-commerce" />
         <div className="p-6">
-          {/* <h2 className="tracking-widest text-2xl title-font font-bold  text-gray-200 mb-1">Title</h2> */}
-          <h1 className="title-font text-lg font-medium text-gray-500 mb-3">E-Commerce Clone</h1>
-          <p className="leading-relaxed mb-3 text-gray-900 text-xl">Technologies Used <span className="text-blue-700">ReactJs, TailwindCSS, Zustand</span></p>
+          <h1 className="title-font text-lg font-medium text-gray-500 mb-3">{items.title}</h1>
+          <p className="leading-relaxed mb-3 text-gray-900 text-xl">Technologies Used <span className="text-blue-700">{items.technology}</span></p>
           <div className="flex items-center flex-wrap gap-36">
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://e-g-shopping-redwin.netlify.app/">Live
+            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href={items.live}>Live
               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M5 12h14"></path>
                 <path d="M12 5l7 7-7 7"></path>
               </svg>
             </a>
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://github.com/redwin-0000/e-commerce">Repo
+            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href={items.Repo}>Repo
               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M5 12h14"></path>
                 <path d="M12 5l7 7-7 7"></path>
@@ -33,56 +40,12 @@ return(
         </div>
       </div>
     </div>
-
-    <div className="p-4 md:w-1/3">
-      <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={auth} alt="blog" />
-        <div className="p-6">
-          {/* <h2 className="tracking-widest text-2xl title-font font-bold  text-gray-200 mb-1">Title</h2> */}
-          <h1 className="title-font text-lg font-medium text-gray-500 mb-3">Authentication with Firebase</h1>
-          <p className="leading-relaxed mb-3 text-gray-900 text-xl">Technologies Used <span className="text-blue-700">ReactJs, CSS, JavaScript</span></p>
-          <div className="flex items-center flex-wrap gap-36">
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://authe-firebase.netlify.app/">Live
-              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://github.com/redwin-0000/signIn">Repo
-              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
+   )
+    })
+  }
     </div>
-
-    <div className="p-4 md:w-1/3">
-      <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={rodeImage} alt="rode-clone" />
-        <div className="p-6">
-          {/* <h2 className="tracking-widest text-2xl title-font font-bold  text-gray-500 mb-1">Title</h2> */}
-          <h1 className="title-font text-lg font-medium text-gray-500 mb-3">Rode Clone</h1>
-          <p className="leading-relaxed mb-3 text-gray-900 text-xl">Technologies Used <span className="text-blue-700"> HTML, TailwindCSS</span></p>
-          <div className="flex items-center flex-wrap gap-36">
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://rode-poj-clone.netlify.app/">Live
-              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <a className="text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" href="https://github.com/redwin-0000/rode-clone">Repo
-              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className="flex  justify-center pt-4">
+    <button className="h-full border-2 bg-blue-600 rounded-md px-2 py-2 overflow-hidden text-white" onClick={()=> loadMore()}>Load More</button>
 
     </div>
   </div>
---
+++ src/components/data.js
@@ -0,0 +1,115 @@
+import img1 from './image/eg.png';
+import img2 from './image/auth.png';
+import img3 from './image/BMI.png';
+import img4 from './image/taxEasy.png';
+import img5 from './image/rode.png';
+import img6 from './image/colorselector.png';
+import img7 from './image/paytm.png';
+import img8 from './image/streetlanding.png';
+import img9 from './image/monstera.png';
+import img10 from './image/shopify.png';
+import img11 from './image/visualDesginer.png';
+import img12 from './image/proj.png';
+
+const data = {
+    cardData:[
+        {
+            id:1,
+            img:img1,
+            title: 'E-Commerce project',
+            technology:'ReactJs, TailwindCSS, Zustand, JavaScript',
+            live:"https://e-g-shopping-redwin.netlify.app/",
+            Repo:"https://github.com/redwin-0000/e-commerce"
+        },
+        {
+            id:2,
+            img:img2,
+            title: 'Authentication with Firebase',
+            technology:'ReactJs, CSS, JavaScript, firebase',
+            live:'',
+            Repo:''
+        },
+        {
+            id:3,
+            img:img3,
+            title: 'Body Mass Index',
+            technology:'React-js, tailwind, JavaScript',
+            live:'',
+            Repo:''
+        },
+        {
+            id:4,
+            img:img4,
+            title: 'TaxEasy',
+            technology:'React-js Tailwind, tailblock, JavaScript',
+            live:'https://taxeasy-clone.netlify.app/',
+            Repo:'https://github.com/redwin-0000/taxeasy'
+        },
+        {
+            id:5,
+            img:img5,
+            title: 'Rode clone',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        },
+        {
+            id:6,
+            img:img6,
+            title: 'UI Color Selectore',
+            desc:'HTML, tailwind, JavaScript',
+            live:'',
+            Repo:''
+        },
+        {
+            id:7,
+            img:img7,
+            title: 'Paytm clone',
+            technology:'HTML, tailwind',
+            live:'',
+            Repo:''
+        },
+        {
+            id:8,
+            img:img8,
+            title: 'Street Landing Page',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        },
+        {
+            id:9,
+            img:img9,
+            title: 'Monstera',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        },
+        {
+            id:10,
+            img:img10,
+            title: 'Monstera',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        },
+        {
+            id:11,
+            img:img11,
+            title: 'Monstera',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        },
+        {
+            id:12,
+            img:img12,
+            title: 'Monstera',
+            technology:'HTML, CSS',
+            live:'',
+            Repo:''
+        }
+    ]
+}
+
+export default data;
