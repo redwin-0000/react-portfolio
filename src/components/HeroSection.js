04516aaa1ab3894bd00efdc08d78c4ca7415f4e7
Ashutosh Singh <109889191+redwin-0000@users.noreply.github.com>
2024-01-05 15:38:25 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection =()=>{
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <Animation />
-      <a className="flex justify-center mt-8" href="https://drive.google.com/file/d/1yPxUMHEq2YehTF0jJ4HVEOZfzqovk94B/view?usp=share_link">
+      <a className="flex justify-center mt-8" href="https://drive.google.com/file/d/1ybS_dVB-dlz1jY_T5WLUWFMvTPj2IfZc/view?usp=sharing">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Resume</button>
       </a>
     </div>
